b4b189631e7de50112abaa572789734e0ca6dd91
Mitchell <mitchelljolly1616@gmail.com>
2019-09-03 13:25:28 +0100

    Implement basic start-unmanaged command which creates a new job on the API

--- command/backup_start_unmanaged.go
+++ command/backup_start_unmanaged.go
@@ -1,6 +1,13 @@
 package command
 
+import "fmt"
 import "github.com/urfave/cli"
+import "time"
+import "log"
+import "net/http"
+import "../config"
+import "../auth"
+import "../actions"
 
 var BackupStartUnmanaged = cli.Command{
 	Name:  "start-unmanaged",
@@ -14,6 +21,20 @@ backupshq start-unmanaged <backup_id> > job-id.txt && ./backup-script.sh | backu
 To run any other type of backup, see backupshq job run --help.
 `,
 	Action: func(c *cli.Context) error {
+		config := config.LoadCli(c)
+
+		tokenResponse, err := auth.GetAccessToken(config)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		client := &http.Client{
+			Timeout: time.Second * 3,
+		}
+		job := actions.StartJob(client, tokenResponse, c.Args().Get(0))
+		fmt.Printf("Started a new job with id %q.\n", job.ID)
+		fmt.Printf("To inform the API when this job is finished run: backupshq finish-unmanaged %q\n", job.ID)
+
 		return nil
 	},
 }
