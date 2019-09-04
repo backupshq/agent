5e1b1ae63a65c0062350797004d3c10df5e347a1
Glynn Forrest <glynn@backbeat.tech>
2019-09-04 12:31:04 +0100

    Command is 'backupshq backup finish-unmanaged'

--- command/backup_start_unmanaged.go
+++ command/backup_start_unmanaged.go
@@ -22,8 +22,8 @@ To run any other type of backup, see backupshq job run --help.
 `,
 	Flags: []cli.Flag{
 		cli.BoolFlag{
-			Name:   "id",
-			Usage:  "Output only the new job ID",
+			Name:  "id",
+			Usage: "Output only the new job ID",
 		},
 	},
 	Action: func(c *cli.Context) error {
@@ -40,7 +40,7 @@ To run any other type of backup, see backupshq job run --help.
 
 		backupID := c.Args().Get(0)
 		backup := actions.GetBackup(client, tokenResponse, backupID)
-		if (backup.Type != actions.BACKUP_TYPE_UNMANAGED) {
+		if backup.Type != actions.BACKUP_TYPE_UNMANAGED {
 			log.Fatal("Cannot start managed backup using start-unmanaged command")
 		}
 
@@ -50,7 +50,7 @@ To run any other type of backup, see backupshq job run --help.
 			return nil
 		}
 		log.Printf("Started a new job with id %q.\n", job.ID)
-		log.Printf("To inform the API when this job is finished run: backupshq finish-unmanaged %q\n", job.ID)
+		log.Printf("To inform the API when this job is finished run: backupshq backup finish-unmanaged %q\n", job.ID)
 
 		return nil
 	},
