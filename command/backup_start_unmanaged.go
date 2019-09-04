fb31da40cd0966909e76ced68e449da1addb6ece
Mitchell <mitchelljolly1616@gmail.com>
2019-09-04 11:28:57 +0100

    Add ID flag to start-unmanaged which reduces output

--- command/backup_start_unmanaged.go
+++ command/backup_start_unmanaged.go
@@ -16,10 +16,16 @@ var BackupStartUnmanaged = cli.Command{
 Start an unmanaged backup.
 This command is only suitable for *unmanaged* backups that you handle yourself, for example:
 
-backupshq start-unmanaged <backup_id> > job-id.txt && ./backup-script.sh | backupshq finish-unmanaged $(cat job-id.txt) --log-stdin
+backupshq start-unmanaged --id <backup_id> > job-id.txt && ./backup-script.sh | backupshq finish-unmanaged $(cat job-id.txt) --log-stdin
 
 To run any other type of backup, see backupshq job run --help.
 `,
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:   "id",
+			Usage:  "Output only the new job ID",
+		},
+	},
 	Action: func(c *cli.Context) error {
 		config := config.LoadCli(c)
 
@@ -39,9 +45,13 @@ To run any other type of backup, see backupshq job run --help.
 		}
 
 		job := actions.StartJob(client, tokenResponse, backupID)
+		if c.Bool("id") {
+			fmt.Printf("%s\n", job.ID)
+			return nil
+		}
 		fmt.Printf("Started a new job with id %q.\n", job.ID)
 		fmt.Printf("To inform the API when this job is finished run: backupshq finish-unmanaged %q\n", job.ID)
-
+		
 		return nil
 	},
 }
