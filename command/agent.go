2042c41968a25b2af46c18bd131a9c256932b6cc
Mitchell <mitchelljolly1616@gmail.com>
2019-09-25 21:37:25 +0100

    Allow sync frequency to be specified as a CLI argument

--- command/agent.go
+++ command/agent.go
@@ -40,8 +40,15 @@ func getLatestSchema(client *http.Client, tokenResponse auth.AccessTokenResponse
 var Agent = cli.Command{
 	Name:  "agent",
 	Usage: "Run the BackupsHQ agent",
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "sync",
+			Value: "* * * * *",
+			Usage: "Cron expression representing how frequently the agent will sync with the API.",
+		},
+	},
 	Action: func(c *cli.Context) error {
-		log.Println("Starting BackupsHQ agent")
+		log.Println("Starting BackupsHQ agent with sync frequency:", c.String("sync"))
 
 		config := config.LoadCli(c)
 
@@ -56,7 +63,7 @@ var Agent = cli.Command{
 
 		backups, crons := getLatestSchema(client, tokenResponse, map[string]actions.Backup{}, map[string]*cron.Cron{})
 		cr := cron.New()
-		cr.AddFunc("* * * * *", func() {
+		cr.AddFunc(c.String("sync"), func() {
 			backups, crons = getLatestSchema(client, tokenResponse, backups, crons)
 		})
 		cr.Start()
