33bb9d2c5890aa50b0cd3f6cafc9694db5aa8a73
Mitchell <mitchelljolly1616@gmail.com>
2019-09-25 21:23:50 +0100

    Extract handler function into named function and call on init

--- command/agent.go
+++ command/agent.go
@@ -13,6 +13,30 @@ import (
 	"github.com/urfave/cli"
 )
 
+func getLatestSchema(client *http.Client, tokenResponse auth.AccessTokenResponse, backups map[string]actions.Backup, crons map[string]*cron.Cron) (map[string]actions.Backup, map[string]*cron.Cron) {
+	log.Println("Checking for changes to backups...")
+	newBackups := actions.ListBackups(client, tokenResponse, actions.BACKUP_TYPE_SCHEDULED)
+	log.Println("Scheduled backups pulled from the API:", len(newBackups))
+
+	didUpdate := false
+	for id := range newBackups {
+		if backups[id] != newBackups[id] {
+			log.Println("Updated backup: " + newBackups[id].Name)
+			didUpdate = true
+			if cron, ok := crons[id]; ok { // checks if there's an existing cron job for this backup
+				cron.Stop()
+			}
+			crons[id] = utils.Schedule(client, tokenResponse, newBackups[id])
+		}
+	}
+
+	if !didUpdate {
+		log.Println("No changes detected")
+	}
+
+	return newBackups, crons
+}
+
 var Agent = cli.Command{
 	Name:  "agent",
 	Usage: "Run the BackupsHQ agent",
@@ -30,32 +54,10 @@ var Agent = cli.Command{
 			Timeout: time.Second * 3,
 		}
 
-		backups := map[string]actions.Backup{}
-		crons := map[string]*cron.Cron{}
-
+		backups, crons := getLatestSchema(client, tokenResponse, map[string]actions.Backup{}, map[string]*cron.Cron{})
 		cr := cron.New()
 		cr.AddFunc("* * * * *", func() {
-			log.Println("Checking for changes to backups...")
-			newBackups := actions.ListBackups(client, tokenResponse, actions.BACKUP_TYPE_SCHEDULED)
-			log.Println("Scheduled backups pulled from the API:", len(newBackups))
-
-			didUpdate := false
-			for id := range newBackups {
-				if backups[id] != newBackups[id] {
-					log.Println("Updated backup: " + newBackups[id].Name)
-					didUpdate = true
-					if cron, ok := crons[id]; ok { // checks if there's an existing cron job for this backup
-						cron.Stop()
-					}
-					crons[id] = utils.Schedule(client, tokenResponse, newBackups[id])
-				}
-			}
-
-			if !didUpdate {
-				log.Println("No changes detected")
-			}
-
-			backups = newBackups
+			backups, crons = getLatestSchema(client, tokenResponse, backups, crons)
 		})
 		cr.Start()
 
