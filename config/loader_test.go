f85a113484a7f865450d063febf3b59df40dd986
Mitchell <mitchelljolly1616@gmail.com>
2019-10-02 16:55:30 +0100

    Add a test for loading env variables in the config

--- config/loader_test.go
+++ config/loader_test.go
@@ -1,10 +1,15 @@
 package config
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestString(t *testing.T) {
+	os.Setenv("TESTVAR", "test")
+	loader := NewConfigLoader()
 	t.Run("load empty configuration", func(t *testing.T) {
-		config, _ := LoadString("")
+		config, _ := loader.LoadString("")
 
 		if config.Auth.ClientId != "" {
 			t.Errorf("got %q want %q", config.Auth.ClientSecret, "")
@@ -12,7 +17,7 @@ func TestString(t *testing.T) {
 	})
 
 	t.Run("load simple config", func(t *testing.T) {
-		config, _ := LoadString(`
+		config, _ := loader.LoadString(`
 [auth]
 client_id = "id"
 client_secret = "secret"
@@ -26,7 +31,7 @@ client_secret = "secret"
 	})
 
 	t.Run("load simple config with odd casing", func(t *testing.T) {
-		config, _ := LoadString(`
+		config, _ := loader.LoadString(`
 [Auth]
 CLIENT_ID = "id"
 Client_Secret = "secret"
@@ -40,7 +45,7 @@ Client_Secret = "secret"
 	})
 
 	t.Run("invalid toml returns error", func(t *testing.T) {
-		config, err := LoadString(`
+		config, err := loader.LoadString(`
 []Not valid
 this doesn't work
 = "
@@ -49,11 +54,24 @@ this doesn't work
 			t.Errorf("error should be returned from bad toml")
 		}
 	})
+
+	t.Run("replace env key with val", func(t *testing.T) {
+		config, _ := loader.LoadString(`
+[auth]
+client_id = "{{ env "TESTVAR"}}"
+client_secret = "secret"
+`)
+
+		if config.Auth.ClientId != "test" {
+			t.Errorf("got %q want %q", config.Auth.ClientId, "test")
+		}
+	})
 }
 
 func TestFile(t *testing.T) {
+	loader := NewConfigLoader()
 	t.Run("load simple file", func(t *testing.T) {
-		config, err := LoadFile("example.toml")
+		config, err := loader.LoadFile("example.toml")
 
 		if err != nil {
 			t.Errorf("file should be loaded successfully")
